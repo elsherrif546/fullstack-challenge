19bf1fa505b9b5b46571d295f933b6e941b51433
Nicholas Boylan <nboylan1pc@gmail.com>
2018-08-01 20:13:13 -0400

    re-format innings.7.json

--- server/database/BoxScore.js
+++ server/database/BoxScore.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 // const db = require('./database.js');
 
 const boxScoreSchema = new mongoose.Schema({
-  // how to design the data?
+  // how to design the data? // 
 });
 
 const BoxScore = mongoose.model('BoxScore', boxScoreSchema);
